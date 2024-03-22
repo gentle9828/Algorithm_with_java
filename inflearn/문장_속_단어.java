6c4c61e6fb51694d11cc66259b0f65dbd25c6dd3
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-22 23:19:58 +0900

    문장 속 단어

--- inflearn/문장_속_단어.java
+++ inflearn/문장_속_단어.java
@@ -1,2 +1,36 @@
-package inflearn;public class 문장_속_단어 {
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 문장_속_단어 {
+    public static String solution(String input) {
+        String answer = "";
+        int min = Integer.MIN_VALUE;
+        String[] s = input.split("");
+        for (String x : s) {
+            int length = x.length();
+            if (length > min) {
+                min = length;
+                answer = x;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+
+        bw.write(solution(input));
+
+        bw.close();
+        br.close();
+    }
 }
