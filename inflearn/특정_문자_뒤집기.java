cec0b481dd729f7e45af11e87c138e9cedfb2adc
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-24 23:38:23 +0900

    특정 문자 뒤집기

---
+++ inflearn/특정_문자_뒤집기.java
@@ -0,0 +1,45 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 특정_문자_뒤집기 {
+    public static String solution(String str) {
+        String answer = "";
+        char[] s = str.toCharArray();
+        int lt = 0, rt = str.length() - 1;
+        while(lt < rt) {
+            if(!Character.isAlphabetic(s[lt])) {
+                lt++;
+            } else if(!Character.isAlphabetic(s[rt])) {
+                rt--;
+            } else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        answer = String.valueOf(s);
+        return answer;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String result = br.readLine();
+
+        bw.write(solution(result));
+        bw.flush();
+
+        bw.close();
+        br.close();
+
+    }
+}
