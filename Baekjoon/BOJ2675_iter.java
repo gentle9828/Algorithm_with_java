2075508f739557b41b0ff61280e30eb422676102
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-27 18:01:17 +0900

    백준 문자열 반복

---
+++ Baekjoon/BOJ2675_iter.java
@@ -0,0 +1,38 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ2675_iter {
+    public static String solution(int R, String S) {
+        String answer = "";
+        for (int i = 0; i < S.length(); i++) {
+            for (int j = 0; j < R; j++) {
+                answer += S.charAt(i);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            String S = st.nextToken();
+            bw.write(solution(R, S) + "\n");
+        }
+
+        bw.close();
+        br.close();
+    }
+}
