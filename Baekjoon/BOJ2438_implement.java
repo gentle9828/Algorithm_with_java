0e37e9931b9332fec8aa159ba323e449e5211df8
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-27 21:37:29 +0900

    백준 별 찍기-1

---
+++ Baekjoon/BOJ2438_implement.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ2438_implement {
+    public static void solution(int N) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        solution(N);
+
+        bw.close();
+        br.close();
+
+    }
+}
