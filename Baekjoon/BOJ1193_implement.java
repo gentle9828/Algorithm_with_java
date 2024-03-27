94e4628984d664b1d5e560f7c10f1100f288d047
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-27 21:12:19 +0900

    백준 파일정리

---
+++ Baekjoon/BOJ1193_implement.java
@@ -0,0 +1,49 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ1193_implement {
+    public static String solution(int X) {
+        String answer = "";
+        int count = 1;
+        int countSum = 0;
+
+        while (true) {
+            if (X <= count + countSum) {
+                if (count % 2 == 1) {
+                    answer = (count - (X - countSum - 1)) + "/" + (X - countSum);
+                    break;
+                }
+
+                else {
+                    answer = (X - countSum) + "/" + (count - (X - countSum - 1));
+                    break;
+                }
+
+            } else {
+                countSum += count;
+                count++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int X = Integer.parseInt(st.nextToken());
+
+        bw.write(solution(X));
+
+        bw.close();
+        br.close();
+    }
+}
