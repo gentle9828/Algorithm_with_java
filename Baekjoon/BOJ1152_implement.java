5969a1aa2a1b3edc36aabc74d664cdacb6491712
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-28 16:48:17 +0900

    백준 단어의 개수

---
+++ Baekjoon/BOJ1152_implement.java
@@ -0,0 +1,21 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ1152_implement {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        bw.write(String.valueOf(st.countTokens()));
+
+        bw.close();
+        br.close();
+    }
+}
