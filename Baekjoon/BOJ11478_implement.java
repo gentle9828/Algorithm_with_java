a6857fc73628b16716dbbda4245cf4ef7e4969eb
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-28 16:31:29 +0900

    서로 다른 부분 문자열의 개수

--- Baekjoon/BOJ11478_implement.java
+++ Baekjoon/BOJ11478_implement.java
@@ -1,2 +1,33 @@
-package Baekjoon;public class BOJ11478_implement {
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ11478_implement {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String input = st.nextToken();
+
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+                set.add(input.substring(i, j));
+            }
+        }
+
+        bw.write(String.valueOf(set.size()));
+
+        bw.close();
+        br.close();
+    }
 }
