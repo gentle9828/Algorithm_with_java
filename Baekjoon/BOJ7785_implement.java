68e71a4e55802864464c3ee09820cfb0fc75f440
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-28 11:38:21 +0900

    백준 회사에 있는 사람

---
+++ Baekjoon/BOJ7785_implement.java
@@ -0,0 +1,49 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ7785_implement {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Set<String> set = new HashSet<>();
+
+        int N = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String key = st.nextToken();
+            String value = st.nextToken();
+
+            if (value.equals("enter")) {
+                set.add(key);
+            } else if (value.equals("leave")) {
+                set.remove(key);
+            }
+        }
+
+        List<String> list = new ArrayList<>(set);
+        Collections.sort(list);
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            bw.write(list.get(i) + "\n");
+        }
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
