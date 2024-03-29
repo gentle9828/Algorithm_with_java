3470e1355a52bb0ca22c490a6a3d06266f47497e
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-29 14:26:28 +0900

    백준 숫자 카드 2

---
+++ Baekjoon/BOJ10816_implement.java
@@ -0,0 +1,41 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ10816_implement {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Map<Integer, Integer> cardMap = new HashMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            cardMap.put(num, cardMap.getOrDefault(num, 0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            int findCard = Integer.parseInt(st.nextToken());
+            sb.append(cardMap.getOrDefault(findCard, 0)).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
