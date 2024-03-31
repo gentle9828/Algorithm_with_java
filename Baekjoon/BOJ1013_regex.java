2569546068caf08edd115468eaf6690f6739f15c
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-03-31 22:56:45 +0900

    백준 Contact

--- Baekjoon/BOJ1013_regex.java
+++ Baekjoon/BOJ1013_regex.java
@@ -5,11 +5,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class BOJ1013 {
+public class BOJ1013_regex {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
