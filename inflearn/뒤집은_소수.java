97a950cf728c7ae1a99817a257659c9ecd59eacf
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-04-27 23:34:54 +0900

    뒤집은 소수

---
+++ inflearn/뒤집은_소수.java
@@ -0,0 +1,63 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class 뒤집은_소수 {
+    public static boolean isPrime(int num) {
+        if(num == 1) {
+            return false;
+        }
+        for(int i = 2; i < num; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static ArrayList<Integer> solution (int n, int[] a) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            int tmp = a[i];
+            int res = 0;
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp = tmp / 10;
+            }
+
+            if (isPrime(res)) {
+                answer.add(res);
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] result = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            result[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int x : solution(N, result)) {
+            bw.write(x + " ");
+        }
+
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
