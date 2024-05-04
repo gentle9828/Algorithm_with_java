e5748a651aeddf1a170e34d324831f960a184a98
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-05-04 22:22:56 +0900

    봉우리

---
+++ inflearn/봉우리.java
@@ -0,0 +1,56 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class 봉우리 {
+    public static int solution(int n, int[][] arr) {
+        int answer = 0;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                boolean flag = true;
+                for(int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) {
+                    answer++;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] result = new int[N][N];
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                result[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        bw.write(String.valueOf(solution(N, result)));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
