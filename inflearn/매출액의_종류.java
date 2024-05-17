92ceb0b90f69a60ae657fc62e8b71b54a337b0da
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2024-05-17 23:18:43 +0900

    매출액의 종류

---
+++ inflearn/매출액의_종류.java
@@ -0,0 +1,59 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class 매출액의_종류 {
+    public static ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < k-1; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int lt = 0;
+        for(int rt = k - 1; rt < n; rt++) {
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+            answer.add(map.size());
+            map.put(arr[lt], map.get(arr[lt]) - 1);
+            if(map.get(arr[lt]) == 0) {
+                map.remove(arr[lt]);
+            }
+            lt++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] result = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            result[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int x : solution(N, K, result)) {
+            bw.write(String.valueOf(x) + " ");
+        }
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
